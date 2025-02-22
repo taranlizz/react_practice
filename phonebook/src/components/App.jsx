a2c006864afa2c03521a96ccd4b1097f4cc6235a
Yelyzaveta Taran <lizataran103@gmail.com>
2025-02-22 21:45:40 +0200

    added storing contacts in localStorage

--- phonebook/src/components/App.jsx
+++ phonebook/src/components/App.jsx
@@ -7,14 +7,22 @@ import { ContactFilter } from "./ContactFilter";
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    if (contacts !== null) {
+      this.setState({ contacts: JSON.parse(contacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
 
   onContactSubmit = (contact) => {
     const isPresent = this.state.contacts.find(
@@ -43,6 +51,7 @@ export class App extends Component {
   render() {
     const { contacts, filter } = this.state;
 
+    // console.log(contacts);
     const normalizedFilter = filter.toLowerCase();
     const filteredContacts = contacts.filter(
       ({ name, number }) =>
